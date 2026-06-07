337729299577b5cd12ec2173d8ed00e115040c72
devj2005 <101204248+devjoshi2005@users.noreply.github.com>
2026-06-07 22:33:59 +0530

    Update code_file.py

--- code_file.py
+++ code_file.py
@@ -174,4 +174,4 @@ if __name__=="__main__":
         
         return jsonify(response_obj)
 
-    app.run(debug=True,host='0.0.0.0',port=8081)
+    app.run(debug=True,host='0.0.0.0',port=8081,threaded=True)
